1247b3915452b5d685e06cd051de9a58ed1fc8a5
martinffg <martinffg@gmail.com>
2016-03-24 03:10:52 -0300

    Avance funcion apertura de RAW en escala de grises

--- src/procesador/domain/ProcesadorDeImagenes.java
+++ src/procesador/domain/ProcesadorDeImagenes.java
@@ -1,12 +1,20 @@
 package procesador.domain;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+
 import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -15,6 +23,8 @@ public class ProcesadorDeImagenes {
 	
     //Imagen actual que se ha cargado
 	private BufferedImage imageActual;
+	private Imagen imagen;	
+	
 	//Método que devuelve una imagen abierta desde archivo
 	//Retorna un objeto BufferedImagen
 	public BufferedImage abrirImagen(){
@@ -41,7 +51,8 @@ public class ProcesadorDeImagenes {
 				if (tipoImagen.equalsIgnoreCase("BMP")){
 					BImg = ImageIO.read(file);
 				} else if (tipoImagen.equalsIgnoreCase("RAW")){
-					BImg = convertirRAWenBufferedImage(convertirRAWaMatriz(file));
+					// hardcodeo imagen raw de columnasXfilas
+					BImg = convertirRAWenBufferedImage(convertirRAWaMatriz(300,200,file));
 				} 		
 				else {
 					System.out.println("ES UN " + tipoImagen);
@@ -66,7 +77,7 @@ public class ProcesadorDeImagenes {
 	private void crearImagen(BufferedImage bufferImage, String extension ) {
 		Integer ancho = bufferImage.getWidth();
 		Integer alto = bufferImage.getHeight();
-		new Imagen(extension, ancho, alto);
+		imagen = new Imagen(extension, ancho, alto);
 	}
 
 	public void guardarImagen(String direccion) throws IOException{
@@ -77,53 +88,82 @@ public class ProcesadorDeImagenes {
 	
 	public BufferedImage convertirRAWenBufferedImage(int[][] rawRGB){
 		
-		int h = rawRGB.length;
-	    int w = rawRGB[0].length;
-	    BufferedImage i = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-	    for (int y=0; y<h; ++y) {
-	        for (int x=0; x<w; ++x) {
+		int h = rawRGB.length;         	System.out.println("filas: "+h);
+	    int w = rawRGB[0].length;		System.out.println("columnas: "+w);
+	    BufferedImage i = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_GRAY);
+	    for (int y=0; y<h; y++) {
+	        for (int x=0; x<w; x++) {
 	            int argb = rawRGB[y][x];
+	            System.out.print(argb+";");
 	            i.setRGB(x, y, argb);
 	        }
+	        System.out.println(" ");
 	    }
 	    return i;
 	}
 	
-	private int[][] convertirRAWaMatriz(File archivoRAW) throws IOException{
+	private int[][] convertirRAWaMatriz(int columnas,int filas,File archivoRAW) throws IOException{
 				
-		FileReader fr = new FileReader(archivoRAW);
-		int cantidadBytesArchivo = (int) archivoRAW.length();
-		Scanner scanner = new Scanner(fr); 
-		String fila="";
-		
-		if (scanner.hasNextLine()){
-			fila = scanner.nextLine();
-		}
-		
-		FileInputStream fileInputStream=null;
-        
-        byte[] bFile = new byte[(int) archivoRAW.length()];
-        
-        //convert file into array of bytes
-	    fileInputStream = new FileInputStream(archivoRAW);
-	    fileInputStream.read(bFile);
-	    fileInputStream.close();
-	    
-	    int columnas = fila.length();
-	    int filas = cantidadBytesArchivo/fila.length();
-        
+		int cantidadBytesArchivo = (int) archivoRAW.length();        
+        byte[] bFile = new byte[cantidadBytesArchivo];
         int[][] rawRGB = new int[filas][columnas];
         
+        //convert file into array of bytes
+        FileInputStream fileInputStream = new FileInputStream(archivoRAW);
+	    fileInputStream.read(bFile);
+	    fileInputStream.close();
+	            
+        /*
         for (int i = 0; i < bFile.length; i++) {
-	       	System.out.print((char)bFile[i]);
+	       	System.out.print((int)bFile[i]+",");
         }
-        int k=0;
-        for (int a=0;a<filas;a++) {
-        	for (int b=0;b<columnas;b++){
-        		rawRGB[a][b]= bFile[k];
-        		k++;
+        System.out.println("");
+        */
+	    
+	    // inicializo la matriz completa en color blanco
+	    for (int i=0;i<columnas;i++){
+	    	for (int j=0;j<filas;j++) {
+	    		rawRGB[j][i]=255;
+	    	}
+	    }
+	    
+	    
+	    System.out.println("Cantidad elementos en bFile: "+bFile.length);
+	    System.out.println("Cantidad bytes en archivo: "+cantidadBytesArchivo);
+	    System.out.println("Cantidad columas*filas: "+columnas*filas);
+	    
+	    
+        int x = 0;
+        int y = 0;
+        int limite = cantidadBytesArchivo;
+        
+        if (cantidadBytesArchivo>(columnas*filas)){
+        	limite= columnas*filas;
+        }
+        
+        System.out.println("Cantidad limite: "+limite);
+        
+        for (int k=0;k<limite;k++) {
+        	
+        	if ((bFile[k]>=0)&&(bFile[k]<255)) {
+        		rawRGB[y][x]= (int) bFile[k];
+        	}else if (bFile[k]<0) {
+        		rawRGB[y][x]= 0;
+        	}else {
+        		rawRGB[y][x]= 255;
         	}
+        	
+        	//System.out.println(k+", x: "+x+", y: "+y+", valor: "+rawRGB[y][x]);
+        	    	
+        	x++;
+        	
+        	if ((k+1)%columnas==0) {
+        		x=0;
+        		y++;
+        	}
+        	
         }
+         
 		return rawRGB;
 	}	
 
