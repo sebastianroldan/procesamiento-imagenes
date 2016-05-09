103da7029797ae942fbc14419c879406922fbdb0
martinffg <martinffg@gmail.com>
2016-05-08 23:22:24 -0300

    Agrego Sliders a Filtros de Prewitt y Sobel

--- src/procesador/domain/Editor.java
+++ src/procesador/domain/Editor.java
@@ -46,6 +46,8 @@ public class Editor extends javax.swing.JFrame implements MouseListener{
 	private JSlider slider = new JSlider(0,255,127);
 	private JSlider sliderContraste = new JSlider(30,225,127);
 	private JSlider sliderPotencia = new JSlider(0,200,100);
+	private JSlider sliderPrewitt =  new JSlider(0,100,50);
+	private JSlider sliderSobel =  new JSlider(0,100,50);
 	private JMenuBar menuBar = new JMenuBar();	
 	private JMenu menuArchivo = new JMenu("Archivo");
 	private JMenuItem itemCargar = new JMenuItem("Abrir Imagen");
@@ -123,7 +125,9 @@ public class Editor extends javax.swing.JFrame implements MouseListener{
     private JLabel valorUmbral = new JLabel();
     private JLabel valorContraste = new JLabel();
     private JLabel valorPotencia = new JLabel();
-	private boolean seleccionando = false;
+    private JLabel valorUmbralPrewitt = new JLabel();
+    private JLabel valorUmbralSobel = new JLabel();
+    private boolean seleccionando = false;
     
 	public Editor() {
 		initComponents();
@@ -221,6 +225,43 @@ public class Editor extends javax.swing.JFrame implements MouseListener{
 				}
 			}
 		});
+		
+		valorUmbralPrewitt = new JLabel("% del Máximo: ");
+		valorUmbralPrewitt.setBounds(1200,30,100,20);
+		valorUmbralPrewitt.setVisible(false);
+		sliderPrewitt.setVisible(false);
+		sliderPrewitt.setBounds(1225, 50, 60, 100);
+		sliderPrewitt.setOrientation(SwingConstants.VERTICAL);
+		valorUmbralPrewitt.setText("% Máximo: "+sliderPrewitt.getValue());
+		sliderPrewitt.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				if (buffer1 != null){
+					borrarHistograma();
+					buffer2 = ObjProcesamiento.pasarFiltroDePrewitt(buffer1, sliderPrewitt.getValue());
+					contenedorDeImagen2.setIcon(new ImageIcon(buffer2));
+					valorUmbralPrewitt.setText("% Máximo: "+sliderPrewitt.getValue());
+				}
+			}
+		});		
+		
+		valorUmbralSobel = new JLabel("% del Máximo: ");
+		valorUmbralSobel.setBounds(1200,30,100,20);
+		valorUmbralSobel.setVisible(false);
+		sliderSobel.setVisible(false);
+		sliderSobel.setBounds(1225, 50, 60, 100);
+		sliderSobel.setOrientation(SwingConstants.VERTICAL);
+		valorUmbralSobel.setText("% Máximo: "+sliderSobel.getValue());
+		sliderSobel.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				if (buffer1 != null){
+					borrarHistograma();
+					buffer2 = ObjProcesamiento.pasarFiltroDePrewitt(buffer1, sliderSobel.getValue());
+					contenedorDeImagen2.setIcon(new ImageIcon(buffer2));
+					valorUmbralSobel.setText("% Máximo: "+sliderSobel.getValue());
+				}
+			}
+		});	
+		
 		this.setLayout(null);
 		this.add(jScrollPane1);
 		this.add(jScrollPane2);
@@ -230,6 +271,10 @@ public class Editor extends javax.swing.JFrame implements MouseListener{
 		this.add(valorContraste);
 		this.add(sliderPotencia);
 		this.add(valorPotencia);
+		this.add(sliderPrewitt);
+		this.add(valorUmbralPrewitt);
+		this.add(sliderSobel);
+		this.add(valorUmbralSobel);
 		mensaje.setBounds(10, 655, 500, 20);
 		this.add(mensaje);
 		this.setExtendedState(MAXIMIZED_BOTH);
@@ -968,7 +1013,19 @@ public class Editor extends javax.swing.JFrame implements MouseListener{
 	private void agregarMenuPrewitt() {
 		itemPrewitt.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				aplicarOperacion(ObjProcesamiento.pasarFiltroDePrewitt(buffer1,obtenerPorcentaje()));
+				if (buffer1 != null){
+					borrarHistograma();
+					buffer2 = ObjProcesamiento.pasarFiltroDePrewitt(buffer1, sliderPrewitt.getValue());
+					contenedorDeImagen2.setIcon(new ImageIcon(buffer2));
+				}
+				if (!sliderPrewitt.isVisible()){
+					valorUmbralPrewitt.setText("% Máximo: "+sliderPrewitt.getValue());
+					valorUmbralPrewitt.setVisible(true);
+					sliderPrewitt.setVisible(true);
+				}else{
+					valorUmbralPrewitt.setVisible(false);
+					sliderPrewitt.setVisible(false);
+				}
 			}
 		});
 	}
@@ -976,7 +1033,18 @@ public class Editor extends javax.swing.JFrame implements MouseListener{
 	private void agregarMenuSobel() {
 		itemSobel.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				aplicarOperacion(ObjProcesamiento.pasarFiltroDeSobel(buffer1,obtenerPorcentaje()));
+				if (buffer1 != null){
+					buffer2 = ObjProcesamiento.pasarFiltroDeSobel(buffer1, sliderSobel.getValue());
+					contenedorDeImagen2.setIcon(new ImageIcon(buffer2));
+				}
+				if (!sliderSobel.isVisible()){
+					valorUmbralSobel.setText("% Máximo: "+sliderSobel.getValue());
+					valorUmbralSobel.setVisible(true);
+					sliderSobel.setVisible(true);
+				}else{
+					valorUmbralSobel.setVisible(false);
+					sliderSobel.setVisible(false);
+				}
 			}
 		});
 	}
